2d78be65bd5e8557b5729954e201f8043d07ae45
Ильдус Садртдинов <ildussadrtdinov@MacBook-Pro-Ildus.local>
2021-03-14 12:44:38 +0300

    adapt resnet architecture

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
-import torchvision.models as models
 from exceptions.exceptions import InvalidBackboneError
+from models.resnet import ResNet18, ResNet50
 
 
 class ResNetSimCLR(nn.Module):
     def __init__(self, base_model, out_dim):
         super(ResNetSimCLR, self).__init__()
-        self.resnet_dict = {"resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
-                            "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)}
+        self.resnet_dict = {"resnet18": ResNet18(num_classes=out_dim),
+                            "resnet50": ResNet50(num_classes=out_dim)}
 
         self.backbone = self._get_basemodel(base_model)
 
